347758496ba061ef4666b09c0d7568753f5f0052
Ryo Hashimoto <ryo.hashimoto.2525@gmail.com>
2023-03-09 09:44:31 +0900

    feat: add multicall contracts

---
+++ packages/multicall/hardhat.config.ts
@@ -0,0 +1,57 @@
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-preprocessor";
+import * as dotenv from 'dotenv';
+import fs from "fs";
+
+dotenv.config();
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+}
+
+module.exports = {
+  zksolc: {
+    version: "1.3.1",
+    compilerSource: "binary",
+    settings: {},
+  },
+  defaultNetwork: "zkSyncTestnet",
+
+  networks: {
+    zkSyncTestnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
+    },
+  },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      },
+    }),
+  },
+  paths: {
+    sources: "./src",
+    tests: "./src/test",
+    cache: "./hardhat-cache",
+    artifacts: './hardhat-artifacts'
+  },
+  solidity: {
+    version: "0.8.17",
+  },
+};
